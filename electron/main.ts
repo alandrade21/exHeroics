2605c0fefb3fc05a61cafcf0768e51600d0ab1dd
André Andrade <alandrade21@gmail.com>
2019-02-07 11:40:35 -0200

    Cleanning code to new component

--- electron/main.ts
+++ electron/main.ts
@@ -1,15 +1,10 @@
 import { DEV_CONFIG_FOLDER_PATH } from './constants';
-import { envMediator } from '@alandrade21/electron-arch';
-import { app } from 'electron';
+import { envDetector } from '@alandrade21/electron-arch';
 import * as configStorage from 'electron-json-storage';
 
-envMediator.printEnvironment();
+envDetector.printEnvironment();
 
-console.log('electron.app.getPath.home', app.getPath('home'));
-console.log('electron.app.getPath.apData', app.getPath('appData'));
-console.log('electron.app.getPath.userData', app.getPath('userData'));
-console.log('jsonStorage default path ', configStorage.getDefaultDataPath());
-console.log('jsonStorage data path ', configStorage.getDataPath());
+
 
 configStorage.setDataPath(`${DEV_CONFIG_FOLDER_PATH}/.config/exHeroics/storage`);
 configStorage.set('teste', {teste: 'conteudo novo'}, (erro: Error) => {
