a5493d4d4247e6b716ee77cbfaa941cbb50c931b
André Andrade <alandrade21@gmail.com>
2019-03-27 14:28:16 -0300

    Delegating window initialization to arch.

--- electron/main.ts
+++ electron/main.ts
@@ -1,25 +1,41 @@
-import { app } from 'electron';
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+import * as url from 'url';
 
-import { MainWindowController } from './mainWindow/MainWindowController';
 import { InitializationController } from './initialization/InitializationController';
 import { DEV_CONFIG_FOLDER_PATH } from './constants';
+import { MainWindowController, envDetector } from '@alandrade21/electron-arch';
 
-app.on('ready', () => {
+try {
+  app.on('ready', () => {
 
-  // It is necessary to create the window first in order to show the dialogs.
-  const mainWindowController = new MainWindowController();
+    // envDetector.printEnvironment();
 
-  // Do the app initialization.
-  try {
-    const initController = new InitializationController('ExHeroics', DEV_CONFIG_FOLDER_PATH);
-    initController.doConfig();
-  } catch (error) {
-    app.quit();
-    return;
-  }
+    // It is necessary to create the window first in order to show the dialogs.
+    MainWindowController.initialize();
 
-  
-});
+    // Do the app initialization.
+    try {
+      console.log('entrei no try');
+      const initController = new InitializationController('ExHeroics', DEV_CONFIG_FOLDER_PATH);
+      initController.doConfig();
+    } catch (error) {
+      console.log('entrei no catch ', error);
+      app.quit();
+      return;
+    }
+
+    console.log('cheguei aqui.');
+    // console.log('main window ', mainWindowController.mainWindow);
+    if (MainWindowController.mainWindow) {
+      MainWindowController.mainWindow.show();
+    }
+  });
+} catch (e) {
+  // Catch Error
+  // throw e;
+  console.log(e);
+}
 
 
 
